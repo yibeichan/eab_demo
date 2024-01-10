0187bf5dc7eddf95b440af7be514fe692eaf5e8d
Yibei Chen <yibeichan@gmail.com>
2024-01-10 10:40:53 -0500

    Update config.js

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -3,6 +3,6 @@ module.exports = {
   githubSrc: 'https://raw.githubusercontent.com/yibeichan/eab_demo/main/eab_demo/eab_demo_schema',
   banner: 'a demo for the eab meeting',
   startButton: 'Join',
-  assetsPublicPath: '/eab_demo_schema/',
+  assetsPublicPath: '/eab_demo/',
   backendServer: null
 };
